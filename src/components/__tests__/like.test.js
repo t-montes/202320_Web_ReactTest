2d54e134fd2b1bed3efb592bcc97fcef84d656a5
t-montes <t.montes@uniandes.edu.co>
2023-10-10 08:39:33 -0500

    change order

--- src/components/__tests__/like.test.js
+++ src/components/__tests__/like.test.js
@@ -6,14 +6,14 @@ test("Expect Likes to be 0 by default", () => {
     expect(screen.getByText("Likes: 0")).toBeInTheDocument();
 });
 
-test("Expect like button to increment likes", () => {
-    render(<Like/>);
-    fireEvent.click(screen.getByText("Like"));
-    expect(screen.getByText("Likes: 1")).toBeInTheDocument();
-});
-
 test("Expect dislike button to decrement likes", () => {
     render(<Like/>);
     fireEvent.click(screen.getByText("Dislike"));
     expect(screen.getByText("Likes: -1")).toBeInTheDocument();
+});
+
+test("Expect like button to increment likes", () => {
+    render(<Like/>);
+    fireEvent.click(screen.getByText("Like"));
+    expect(screen.getByText("Likes: 1")).toBeInTheDocument();
 });
