e6bd8227cf47b71ebbc9341922608f1c9970edeb
t-montes <t.montes@uniandes.edu.co>
2023-10-10 08:42:21 -0500

    ;

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import Checkbox from './components/checkbox';
-import Like from './components/likes';
+import Like from './components/like';
 
 const container = document.getElementById("root");
 const root = createRoot(container);
